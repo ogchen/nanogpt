32d21ff2793c64402c50d1b32351a5f1423c142f
Oscar Chen <oscarguoyinchen@gmail.com>
2024-02-12 13:20:58 +0000

    Add token encoder and decoder

--- nanogpt.py
+++ nanogpt.py
@@ -1,22 +1,42 @@
 import argparse
 
+
+def generate_encoder_decoder(token_list):
+    token_to_encoding = {c: i for i, c in enumerate(token_list)}
+    encoding_to_token = {i: c for i, c in enumerate(token_list)}
+
+    encode = lambda tokens: list(map(lambda t: token_to_encoding[t], tokens))
+    decode = lambda encodings: list(map(lambda e: encoding_to_token[e], encodings))
+
+    return encode, decode
+
+
+def extract_token_list(text):
+    return sorted(list(set(text)))
+
+
 def read_file(filename):
     with open(filename, mode="r") as f:
         text = f.read()
     return text
 
+
 def parse_args():
     parser = argparse.ArgumentParser(
-        prog="nanogpt",
-        description="Executes a GPT language model"
+        prog="nanogpt", description="Executes a GPT language model"
     )
     parser.add_argument("filename")
     return parser.parse_args()
 
+
 def main():
     args = parse_args()
     content = read_file(args.filename)
-    print(content[:1000])
+    token_list = extract_token_list(content)
+    encode, decode = generate_encoder_decoder(token_list)
+    print(encode("Hello world"))
+    print("".join(decode(encode("Hello world"))))
+
 
 if __name__ == "__main__":
     main()
