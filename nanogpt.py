1de8a1bdbf3c047e69a1d5197c5ae3d196a2ea08
Oscar Chen <oscarguoyinchen@gmail.com>
2024-02-12 13:37:52 +0000

    Limit total number of tokens to max uint8

--- nanogpt.py
+++ nanogpt.py
@@ -1,18 +1,21 @@
 import argparse
+import torch
+
+MAX_TOKENS = 255  # Allow usage of uint8
 
 
 def generate_encoder_decoder(token_list):
     token_to_encoding = {c: i for i, c in enumerate(token_list)}
     encoding_to_token = {i: c for i, c in enumerate(token_list)}
 
-    encode = lambda tokens: [token_to_encoding[t] for t in tokens]
+    encode = lambda tokens: [token_to_encoding[t] for t in tokens if t in token_list]
     decode = lambda encodings: "".join((encoding_to_token[e] for e in encodings))
 
     return encode, decode
 
 
-def extract_token_list(text):
-    return sorted(list(set(text)))
+def extract_token_universe(text, max_tokens):
+    return sorted(list(set(text)))[:max_tokens]
 
 
 def read_file(filename):
@@ -32,10 +35,11 @@ def parse_args():
 def main():
     args = parse_args()
     content = read_file(args.filename)
-    token_list = extract_token_list(content)
-    encode, decode = generate_encoder_decoder(token_list)
-    print(encode("Hello world"))
-    print(decode(encode("Hello world")))
+    token_universe = extract_token_universe(content, MAX_TOKENS)
+    encode, decode = generate_encoder_decoder(token_universe)
+    content_encoding = torch.tensor(encode(content), dtype=torch.uint8)
+    print(content_encoding[:1000])
+    print(decode(encode("hello worlddd")))
 
 
 if __name__ == "__main__":
