a81174c436bb2e04ae25bf8ba565bac2d95e11d7
Oscar Chen <oscarguoyinchen@gmail.com>
2024-02-12 16:15:09 +0000

    Add generation of batches of blocks

--- nanogpt.py
+++ nanogpt.py
@@ -3,6 +3,16 @@ import torch
 
 MAX_TOKENS = 255  # Allow usage of uint8
 TRAINING_DATA_PERCENTAGE = 0.9
+BLOCK_SIZE = 8
+BATCH_SIZE = 4
+
+
+def generate_batch(data):
+    assert len(data) >= BLOCK_SIZE
+    offsets = torch.randint(len(data) - BLOCK_SIZE + 1, (BATCH_SIZE,))
+    input_batch = torch.stack([data[offset:offset + BLOCK_SIZE] for offset in offsets])
+    target_batch = torch.stack([data[offset + 1: offset + 1 + BLOCK_SIZE] for offset in offsets])
+    return input_batch, target_batch
 
 
 def split_data(data, training_data_percentage):
@@ -47,6 +57,9 @@ def main():
     encode, decode = generate_encoder_decoder(token_universe)
     data = torch.tensor(encode(content), dtype=torch.uint8)
     train_data, val_data = split_data(data, TRAINING_DATA_PERCENTAGE)
+    input, target = generate_batch(train_data)
+    print("input", input)
+    print("target", target)
 
 
 if __name__ == "__main__":
