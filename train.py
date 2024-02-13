cb1fe5d7d99b13c1f74cbae7a194fcc478e72a51
Oscar Chen <oscarguoyinchen@gmail.com>
2024-02-13 17:21:55 +0000

    Complete transformer implementation

--- train.py
+++ train.py
@@ -6,16 +6,20 @@ from bigram import BigramLanguageModel
 from transformer import TransformerLanguageModel
 
 TRAINING_DATA_PERCENTAGE = 0.9
-BLOCK_SIZE = 8
-BATCH_SIZE = 4
-EMBEDDING_DIMENSIONS = 32
-HEAD_SIZE = 16
-DEVICE = "cpu"
-# DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BLOCK_SIZE = 256
+BATCH_SIZE = 64
+NUM_HEADS = 6
+# EMBEDDING_DIMENSIONS = NUM_HEADS * 64
+# NUM_BLOCKS = 6
+EMBEDDING_DIMENSIONS = NUM_HEADS * 2
+NUM_BLOCKS = 2
+# DEVICE = "cpu"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 EVALUATION_ITERATIONS = 200
-PRINT_ITERATIONS = 5000
-TOTAL_ITERATIONS = 20000
-LEARNING_RATE = 1e-3
+PRINT_ITERATIONS = 20
+TOTAL_ITERATIONS = 5000
+LEARNING_RATE = 3e-4
+DROPOUT = 0.2
 
 
 def timing_decorator(func):
@@ -42,14 +46,19 @@ def estimate_loss(data, model):
     return losses.mean()
 
 
-def print_training_status(iteration, train_data, val_data, model, encode, decode):
+def print_loss(iteration, train_data, val_data, model):
     train_loss = estimate_loss(train_data, model)
     val_loss = estimate_loss(val_data, model)
+    print(f"iteration: {iteration}, training: {train_loss}, val loss: {val_loss}")
+
+
+def print_sample_output(model, encode, decode):
+    model.eval()
     generated = model.generate(
         torch.tensor(encode(" "), dtype=torch.long, device=DEVICE)[None, :],
         num_new_tokens=100,
     )
-    print(f"iteration: {iteration}, training: {train_loss}, val loss: {val_loss}")
+    model.train()
     print("sample output:")
     print(decode(generated[0].tolist()))
     print()
@@ -57,7 +66,7 @@ def print_training_status(iteration, train_data, val_data, model, encode, decode
 
 @timing_decorator
 def train_model(train_data, val_data, model, optimizer, encode, decode, iterations):
-    for i in range(iterations):
+    for i in range(iterations + 1):
         inputs, targets = sample_batch(train_data)
         _, loss = model(inputs, targets)
         optimizer.zero_grad()
@@ -65,7 +74,8 @@ def train_model(train_data, val_data, model, optimizer, encode, decode, iteratio
         optimizer.step()
 
         if i % PRINT_ITERATIONS == 0:
-            print_training_status(i, train_data, val_data, model, encode, decode)
+            print_loss(i, train_data, val_data, model)
+    print_sample_output(model, encode, decode)
 
 
 def sample_batch(data):
@@ -127,12 +137,13 @@ def main():
     encode, decode = generate_encoder_decoder(token_universe)
     data = torch.tensor(encode(content), dtype=torch.long)
     train_data, val_data = split_data(data, TRAINING_DATA_PERCENTAGE)
-    # model = BigramLanguageModel(len(token_universe))
     model = TransformerLanguageModel(
         max_tokens=len(token_universe),
         max_block_size=BLOCK_SIZE,
         embedding_dimensions=EMBEDDING_DIMENSIONS,
-        head_size=HEAD_SIZE,
+        num_heads=NUM_HEADS,
+        num_transformer_blocks=NUM_BLOCKS,
+        dropout=DROPOUT,
     )
     model = model.to(DEVICE)
     optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
--- transformer.py
+++ transformer.py
@@ -3,8 +3,8 @@ from torch import nn
 from torch.nn import functional
 
 
-class AttentionHead(nn.Module):
-    def __init__(self, embedding_size, head_size, max_block_size):
+class HeadAttention(nn.Module):
+    def __init__(self, embedding_size, head_size, max_block_size, dropout):
         super().__init__()
         self.key_model = nn.Linear(embedding_size, head_size, bias=False)
         self.query_model = nn.Linear(embedding_size, head_size, bias=False)
@@ -12,44 +12,114 @@ class AttentionHead(nn.Module):
         self.register_buffer(
             "tril", torch.tril(torch.ones(max_block_size, max_block_size))
         )
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, embeddings):
         _, block_size, _ = embeddings.shape  # B, T, C
         key = self.key_model(embeddings)  # B, T, 16
         query = self.query_model(embeddings)  # B, T, 16
-        normalisation_factor = key.shape[-1]**-0.5
+        normalisation_factor = key.shape[-1] ** -0.5
         weights = query @ key.transpose(-2, -1) * normalisation_factor
         weights = weights.masked_fill(
             self.tril[:block_size, :block_size] == 0, value=float("-inf")
         )
         weights = functional.softmax(weights, dim=-1)
+        weights = self.dropout(weights)
         value = self.value_model(embeddings)  # B, T, 16
 
         return weights @ value
 
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, embedding_size, head_size, max_block_size, dropout):
+        super().__init__()
+        self.multi_head = nn.ModuleList(
+            [
+                HeadAttention(embedding_size, head_size, max_block_size, dropout)
+                for _ in range(num_heads)
+            ]
+        )
+        self.projection = nn.Linear(embedding_size, embedding_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = torch.cat([head(x) for head in self.multi_head], dim=-1)
+        x = self.projection(x)
+        x = self.dropout(x)
+        return x
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, dropout, dim_multiplier=4):
+        super().__init__()
+        self.feed_forward = nn.Sequential(
+            nn.Linear(dim, dim * dim_multiplier),
+            nn.ReLU(),
+            nn.Linear(dim * dim_multiplier, dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, inputs):
+        return self.feed_forward(inputs)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, max_block_size, embedding_dimensions, num_heads, dropout):
+        super().__init__()
+        self.multi_attention = MultiHeadAttention(
+            num_heads=num_heads,
+            embedding_size=embedding_dimensions,
+            head_size=embedding_dimensions // num_heads,
+            max_block_size=max_block_size,
+            dropout=dropout,
+        )
+        self.feed_forward = FeedForward(embedding_dimensions, dropout)
+        self.layer_norm1 = nn.LayerNorm(embedding_dimensions)
+        self.layer_norm2 = nn.LayerNorm(embedding_dimensions)
+
+    def forward(self, x):
+        x = x + self.multi_attention(self.layer_norm1(x))
+        x = x + self.feed_forward(self.layer_norm2(x))
+        return x
+
+
 class TransformerLanguageModel(nn.Module):
-    def __init__(self, max_tokens, max_block_size, embedding_dimensions, head_size):
+    def __init__(
+        self,
+        max_tokens,
+        max_block_size,
+        embedding_dimensions,
+        num_heads,
+        num_transformer_blocks,
+        dropout,
+    ):
         super().__init__()
         self.max_block_size = max_block_size
         self.register_buffer("positions", torch.arange(max_block_size))
         self.token_embeddings = nn.Embedding(max_tokens, embedding_dimensions)
         self.position_embeddings = nn.Embedding(max_block_size, embedding_dimensions)
-        self.attention_head = AttentionHead(
-            embedding_size=embedding_dimensions,
-            head_size=head_size,
-            max_block_size=max_block_size,
+        self.transformer_blocks = nn.Sequential(
+            *[
+                TransformerBlock(
+                    max_block_size=max_block_size,
+                    embedding_dimensions=embedding_dimensions,
+                    num_heads=num_heads,
+                    dropout=dropout,
+                )
+                for _ in range(num_transformer_blocks)
+            ],
+            nn.LayerNorm(embedding_dimensions)
         )
-        self.language_model_head = nn.Linear(head_size, max_tokens)
+        self.language_model_head = nn.Linear(embedding_dimensions, max_tokens)
 
     def forward(self, inputs, targets=None):
         inputs = inputs[:, -self.max_block_size :]
         batch_size, block_size = inputs.shape
         token_embeddings = self.token_embeddings(inputs)
         position_embeddings = self.position_embeddings(self.positions[:block_size])
-        embeddings = token_embeddings + position_embeddings
-        attention = self.attention_head(embeddings)
-        logits = self.language_model_head(attention)
+        x = token_embeddings + position_embeddings
+        x = self.transformer_blocks(x)
+        logits = self.language_model_head(x)
         if targets is None:
             loss = None
         else:
