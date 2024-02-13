076527379be6c4ae2bee1e5a0821196ccea3ffbb
Oscar Chen <oscarguoyinchen@gmail.com>
2024-02-13 18:42:15 +0000

    Add model saving and loading

--- train.py
+++ train.py
@@ -1,22 +1,30 @@
 import torch
 import time
+import signal
+import sys
+import os
 import argparse
 from functools import wraps
 from bigram import BigramLanguageModel
 from transformer import TransformerLanguageModel
 
+
+MODEL = None
+CURRENT_ITERATION = 0
+OPTIMIZER = None
+
+
 TRAINING_DATA_PERCENTAGE = 0.9
 BLOCK_SIZE = 256
 BATCH_SIZE = 64
 NUM_HEADS = 6
-# EMBEDDING_DIMENSIONS = NUM_HEADS * 64
-# NUM_BLOCKS = 6
-EMBEDDING_DIMENSIONS = NUM_HEADS * 2
-NUM_BLOCKS = 2
-# DEVICE = "cpu"
+EMBEDDING_DIMENSIONS = NUM_HEADS * 64
+NUM_BLOCKS = 6
+# EMBEDDING_DIMENSIONS = NUM_HEADS * 2
+# NUM_BLOCKS = 2
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 EVALUATION_ITERATIONS = 200
-PRINT_ITERATIONS = 20
+PRINT_ITERATIONS = 5
 TOTAL_ITERATIONS = 5000
 LEARNING_RATE = 3e-4
 DROPOUT = 0.2
@@ -46,10 +54,11 @@ def estimate_loss(data, model):
     return losses.mean()
 
 
-def print_loss(iteration, train_data, val_data, model):
+def print_loss(train_data, val_data, model):
+    global CURRENT_ITERATION
     train_loss = estimate_loss(train_data, model)
     val_loss = estimate_loss(val_data, model)
-    print(f"iteration: {iteration}, training: {train_loss}, val loss: {val_loss}")
+    print(f"iteration: {CURRENT_ITERATION}, training: {train_loss}, val loss: {val_loss}")
 
 
 def print_sample_output(model, encode, decode):
@@ -66,16 +75,17 @@ def print_sample_output(model, encode, decode):
 
 @timing_decorator
 def train_model(train_data, val_data, model, optimizer, encode, decode, iterations):
+    global CURRENT_ITERATION
     for i in range(iterations + 1):
         inputs, targets = sample_batch(train_data)
         _, loss = model(inputs, targets)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
         if i % PRINT_ITERATIONS == 0:
-            print_loss(i, train_data, val_data, model)
-    print_sample_output(model, encode, decode)
+            print_loss(train_data, val_data, model)
+            #print_sample_output(model, encode, decode)
+        CURRENT_ITERATION += 1
 
 
 def sample_batch(data):
@@ -126,18 +136,52 @@ def parse_args():
     parser = argparse.ArgumentParser(
         prog="nanogpt", description="Executes a GPT language model"
     )
+    parser.add_argument("-s", "--statefile")
     parser.add_argument("filename")
     return parser.parse_args()
 
 
+def save_checkpoint(filepath):
+    global CURRENT_ITERATION
+    global MODEL
+    global OPTIMIZER
+    if MODEL is not None and OPTIMIZER is not None:
+        torch.save({
+            "iteration": CURRENT_ITERATION,
+            "model_state": MODEL.state_dict(),
+            "optimizer_state": OPTIMIZER.state_dict(),
+        }, filepath)
+
+
+def load_checkpoint(filepath):
+    global CURRENT_ITERATION
+    global MODEL
+    global OPTIMIZER
+    if os.path.isfile(filepath):
+        checkpoint = torch.load(filepath)
+        MODEL.load_state_dict(checkpoint["model_state"])
+        OPTIMIZER.load_state_dict(checkpoint["optimizer_state"])
+        CURRENT_ITERATION = checkpoint["iteration"]
+        print(f"Loaded state file from {filepath}, iteration: {CURRENT_ITERATION}")
+
+
 def main():
+    global CURRENT_ITERATION
+    global MODEL
+    global OPTIMIZER
+
     args = parse_args()
+    def signal_handler(*_):
+        save_checkpoint(args.statefile)
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+
     content = read_file(args.filename)
     token_universe = extract_token_universe(content)
     encode, decode = generate_encoder_decoder(token_universe)
     data = torch.tensor(encode(content), dtype=torch.long)
     train_data, val_data = split_data(data, TRAINING_DATA_PERCENTAGE)
-    model = TransformerLanguageModel(
+    MODEL = TransformerLanguageModel(
         max_tokens=len(token_universe),
         max_block_size=BLOCK_SIZE,
         embedding_dimensions=EMBEDDING_DIMENSIONS,
@@ -145,13 +189,14 @@ def main():
         num_transformer_blocks=NUM_BLOCKS,
         dropout=DROPOUT,
     )
-    model = model.to(DEVICE)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+    MODEL = MODEL.to(DEVICE)
+    OPTIMIZER = torch.optim.AdamW(MODEL.parameters(), lr=LEARNING_RATE)
+    load_checkpoint(args.statefile)
     train_model(
         train_data,
         val_data,
-        model,
-        optimizer,
+        MODEL,
+        OPTIMIZER,
         encode,
         decode,
         iterations=TOTAL_ITERATIONS,
